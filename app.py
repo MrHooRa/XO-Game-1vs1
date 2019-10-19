f529fb52907a5dc214402bed35f4ba032a5de1d1
Salman <50276554+MrHooRa@users.noreply.github.com>
2019-10-19 23:33:40 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #
 # XO Game
+# By using array and some AI
 #
 
 players = {
-    "player1": "Player 1",
-    "player2": "Player 2"
+    "player1": "Knight",
+    "player2": "FireMaN"
 }
 
 game = {
@@ -22,8 +23,12 @@ game = {
 
 
 def start():
-    players["player1"] = input("\n\tPlayer 1 nickname: ")
-    players["player2"] = input("\n\tPlayer 2 nickname: ")
+    player_1_nickname = input("\n\tPlayer 1 nickname: ")
+    player_2_nickname = input("\n\tPlayer 2 nickname: ")
+    if player_1_nickname != "":
+        players["player1"] = player_1_nickname
+    if player_2_nickname != "":
+        players["player2"] = player_2_nickname
 
 
 def game_board():
@@ -37,60 +42,77 @@ def check(player):
     else:
         if game[9] == "X" and game[6] == "X" and game[3] == "X":
             print("\n\n\t\t {0} win!\n\n".format(players['player1']))
-            game["start"] = 0
+            game["start"] = 2
         if game[8] == "X" and game[5] == "X" and game[2] == "X":
             print("\n\n\t\t {0} win!\n\n".format(players['player1']))
-            game["start"] = 0
+            game["start"] = 2
         if game[7] == "X" and game[4] == "X" and game[1] == "X":
             print("\n\n\t\t {0} win!\n\n".format(players['player1']))
-            game["start"] = 0
+            game["start"] = 2
         if game[9] == "X" and game[8] == "X" and game[7] == "X":
             print("\n\n\t\t {0} win!\n\n".format(players['player1']))
-            game["start"] = 0
+            game["start"] = 2
         if game[6] == "X" and game[5] == "X" and game[4] == "X":
             print("\n\n\t\t {0} win!\n\n".format(players['player1']))
-            game["start"] = 0
+            game["start"] = 2
         if game[3] == "X" and game[2] == "X" and game[1] == "X":
             print("\n\n\t\t {0} win!\n\n".format(players['player1']))
-            game["start"] = 0
+            game["start"] = 2
         if game[3] == "X" and game[2] == "X" and game[1] == "X":
             print("\n\n\t\t {0} win!\n\n".format(players['player1']))
-            game["start"] = 0
+            game["start"] = 2
         if game[9] == "X" and game[5] == "X" and game[1] == "X":
             print("\n\n\t\t {0} win!\n\n".format(players['player1']))
-            game["start"] = 0
+            game["start"] = 2
         if game[3] == "X" and game[5] == "X" and game[7] == "X":
             print("\n\n\t\t {0} win!\n\n".format(players['player1']))
-            game["start"] = 0
+            game["start"] = 2
         if game[9] == "O" and game[6] == "O" and game[3] == "O":
             print("\n\n\t\t {0} win!\n\n".format(players['player2']))
-            game["start"] = 0
+            game["start"] = 2
         if game[8] == "O" and game[5] == "O" and game[2] == "O":
             print("\n\n\t\t {0} win!\n\n".format(players['player2']))
-            game["start"] = 0
+            game["start"] = 2
         if game[7] == "O" and game[4] == "O" and game[1] == "O":
             print("\n\n\t\t {0} win!\n\n".format(players['player2']))
-            game["start"] = 0
+            game["start"] = 2
         if game[9] == "O" and game[8] == "O" and game[7] == "O":
             print("\n\n\t\t {0} win!\n\n".format(players['player2']))
-            game["start"] = 0
+            game["start"] = 2
         if game[6] == "O" and game[5] == "O" and game[4] == "O":
             print("\n\n\t\t {0} win!\n\n".format(players['player2']))
-            game["start"] = 0
+            game["start"] = 2
         if game[3] == "O" and game[2] == "O" and game[1] == "O":
             print("\n\n\t\t {0} win!\n\n".format(players['player2']))
-            game["start"] = 0
+            game["start"] = 2
         if game[3] == "O" and game[2] == "O" and game[1] == "O":
             print("\n\n\t\t {0} win!\n\n".format(players['player2']))
-            game["start"] = 0
+            game["start"] = 2
         if game[9] == "O" and game[5] == "O" and game[2] == "O":
             print("\n\n\t\t {0} win!\n\n".format(players['player2']))
-            game["start"] = 0
+            game["start"] = 2
         if game[3] == "O" and game[5] == "O" and game[7] == "O":
             print("\n\n\t\t {0} win!\n\n".format(players['player2']))
-            game["start"] = 0
-        if game["start"] == 0:
+            game["start"] = 2
+        if game["start"] == 2:
             print("I hope you enjoyed :D")
+            action = input("Play again? Yes[1] No[2]: ")
+            if action == "Yes" or action == "yes":
+                game[1] = '-'
+                game[2] = '-'
+                game[3] = '-'
+                game[4] = '-'
+                game[5] = '-'
+                game[6] = '-'
+                game[7] = '-'
+                game[8] = '-'
+                game[9] = '-'
+                game_board()
+                game["start"] = 1
+                move(1)
+            else:
+                print("Thank you for playing my game :D <3")
+                exit
         else:
             move(player)
 
@@ -98,26 +120,40 @@ def check(player):
 def move(player):
     if player == 1:
         if game['start'] == 1:
-            player_choose = input("\n\t{0} round: ".format(players['player1']))
-            player_choose = int(player_choose)
-            if player_choose < 10:
-                if game[player_choose]:
-                    game[player_choose] = 'X'
+            box = input("\n\t{0} round (X): ".format(players['player1']))
+            if box.isdigit() and box != "":
+                box = int(box)
+            else:
+                print("\t\tTry again!")
+                move(player)
+            if box < 10:
+                if game[box] == "-":
+                    game[box] = 'X'
+                else:
+                    print("\t\tYou can NOT choose this box!")
+                    move(player)
             game_board()
             check(2)
     if player == 2:
         if game['start'] == 1:
-            player_choose = input("\n\t{0} round: ".format(players['player2']))
-            player_choose = int(player_choose)
-            if player_choose < 10:
-                if game[player_choose]:
-                    game[player_choose] = 'O'
+            box = input("\n\t{0} round (O): ".format(players['player2']))
+            if box.isdigit() and box != "":
+                box = int(box)
+            else:
+                print("\t\tTry again!")
+                move(player)
+            if box < 10:
+                if game[box] == "-":
+                    game[box] = 'O'
+                else:
+                    print("\t\tYou can NOT choose this box!")
+                    move(player)
             game_board()
             check(1)
 
 
 if game['start'] == 0:
-    print("\n\t\tWelcome To start please choose which box you want!\n")
+    print("\n\t\t\t\tWelcome To XO Game\n")
     start()
     game_board()
     game["start"] = 1
